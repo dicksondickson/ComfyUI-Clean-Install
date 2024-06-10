3c238c092b5991fdece39eebb8412538cd7cd5a0
Dickson <dicksondickson@users.noreply.github.com>
2024-06-10 03:02:10 -0700

    Add files via upload

--- flash-attn-compile.bat
+++ flash-attn-compile.bat
@@ -1,12 +1,23 @@
-:: Install comfyui and torch first
+:: Compile Flash attention
+:: Only needed if you want to the use the latest and greatest
+
+:: Install torch nightly
+pip uninstall torch
+pip install --pre torch torchvision torchaudio --index-url https://download.pytorch.org/whl/nightly/cu124
 
 pip install packaging
 pip install wheel
+
+:: Install ninja for faster compile
 pip uninstall -y ninja && pip install ninja
 
+:: clone flash attention
 git clone https://github.com/Dao-AILab/flash-attention.git
 cd flash-attention
-set MAX_JOBS=10
-python setup.py install
 
+:: set the number of threads. 10 jobs uses 60gigs of ram at peak
+set MAX_JOBS=10
+
+:: start compile
+python setup.py install
 pause
