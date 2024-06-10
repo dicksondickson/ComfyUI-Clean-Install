7c51c5ff99cc9b6188eb7666d93db85c24431317
Dickson <dicksondickson@users.noreply.github.com>
2024-06-10 02:42:54 -0700

    Add files via upload

---
+++ flash-attn-compile.bat
@@ -0,0 +1,10 @@
+pip install packaging
+pip install wheel
+pip uninstall -y ninja && pip install ninja
+
+git clone https://github.com/Dao-AILab/flash-attention.git
+cd flash-attention
+set MAX_JOBS=10
+python setup.py install
+
+pause
