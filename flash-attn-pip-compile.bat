c55055b888a9b7b2afefd1c0c7815b9d5608313f
Dickson <dickson@dicksonchow.com>
2024-07-25 13:43:02 -0700

    update to new torch

--- flash-attn-pip-compile.bat
+++ flash-attn-pip-compile.bat
@@ -1,13 +1,14 @@
 :: Compile Flash attention
-:: Only needed if you want to the use the latest and greatest
+:: NOT RECOMMENDED
 
 
 :: Install torch nightly
-:: pip uninstall torch
-:: pip install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu121
+:: pip uninstall torch torchvision torchaudio
+:: pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu124
 pip install --pre torch torchvision torchaudio --index-url https://download.pytorch.org/whl/nightly/cu124
 
 
+:: Install dependencies
 pip install packaging
 pip install wheel
 
@@ -16,9 +17,8 @@ pip install wheel
 pip uninstall -y ninja && pip install ninja
 
 
-
 :: set the number of threads. 10 jobs uses 60gigs of ram at peak
-set MAX_JOBS=9
+set MAX_JOBS=4
 
 
 :: start compile
@@ -27,5 +27,6 @@ pip install flash-attn --no-build-isolation
 
 :: build wheel
 :: python setup.py bdist_wheel
+
 
 pause
